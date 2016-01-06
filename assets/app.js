a677da06d2b26221aaeb8f2c0ad99729b1759df3
Zhengyu Qian <laerhsif@gmail.com>
2016-01-06 20:57:05 +0800

    Repeat the animation once after 2s delaying.

--- assets/app.js
+++ assets/app.js
@@ -2,4 +2,10 @@
     'use strict';
     var $yellow = document.getElementById('yellow');
     $yellow.classList.add('animate');
+    setTimeout(function () {
+        $yellow.classList.remove('animate');
+        setTimeout(function () {
+            $yellow.classList.add('animate');
+        }, 5);
+    }, 5005);
 })();
--- typescript/app.ts
+++ typescript/app.ts
@@ -2,6 +2,15 @@
 	'use strict';
 
 	var $yellow = document.getElementById('yellow');
+	
 	$yellow.classList.add('animate');
+	
+	setTimeout(function() {
+		$yellow.classList.remove('animate');
+		
+		setTimeout(function() {
+			$yellow.classList.add('animate');
+		}, 5);
+	}, 5005);
 
 })();
